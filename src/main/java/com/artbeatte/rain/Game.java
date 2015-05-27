d46c51d8074c7ed92415f06648c2e04be6b110c7
Art Beatte <art.beatte@docusign.com>
2015-05-27 09:35:55 -0700

    Codes up through Episode 17 of Game Programming.

--- src/main/java/com/artbeatte/rain/Game.java
+++ src/main/java/com/artbeatte/rain/Game.java
@@ -1,6 +1,7 @@
 package com.artbeatte.rain;
 
 import com.artbeatte.rain.graphics.Screen;
+import com.artbeatte.rain.input.Keyboard;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,6 +23,7 @@ public class Game extends Canvas implements Runnable {
 
     private Thread thread;
     private JFrame frame;
+    private Keyboard key;
     private boolean running = false;
 
     private Screen screen;
@@ -34,8 +36,10 @@ public class Game extends Canvas implements Runnable {
         setPreferredSize(size);
 
         screen = new Screen(width, height);
-
         frame = new JFrame();
+        key = new Keyboard();
+
+        addKeyListener(key);
     }
 
     public synchronized void start() {
@@ -83,7 +87,7 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void update() {
-
+        key.update();
     }
 
     public void render() {
