785de9470a874c0bffabe4c7578652d93a893231
Art Beatte <art.beatte@docusign.com>
2015-09-03 15:52:23 -0700

    Codes up through Episode 41 of Game Programming.

---
+++ src/main/java/com/artbeatte/rain/entity/mob/Player.java
@@ -0,0 +1,22 @@
+package com.artbeatte.rain.entity.mob;
+
+/**
+ * @author art.beatte
+ * @version 9/3/15
+ */
+public class Player extends Mob {
+
+    public Player() {
+    }
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() {
+    }
+
+    public void render() {
+    }
+}
