6a86d6ab1dde69c506dd8ff73740f91f57969b58
Art Beatte <art.beatte@docusign.com>
2015-09-03 08:45:50 -0700

    Codes up through Episode 19 of Game Programming.

---
+++ src/main/java/com/artbeatte/rain/graphics/SpriteSheet.java
@@ -0,0 +1,34 @@
+package com.artbeatte.rain.graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @author art.beatte
+ * @version 9/3/15
+ */
+public class SpriteSheet {
+
+    private String path;
+    private final int SIZE;
+    public int[] pixels;
+
+    public SpriteSheet(String path, int size) {
+        this.path = path;
+        SIZE = size;
+        pixels = new int[SIZE * SIZE];
+        load();
+    }
+
+    private void load() {
+        try {
+            BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0, 0, w, h, pixels, 0, w);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
