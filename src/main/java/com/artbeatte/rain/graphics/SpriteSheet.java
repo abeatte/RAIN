e0e259db0be8b5b7159574f781ca4d61a962ae37
Art Beatte <art.beatte@docusign.com>
2015-09-03 08:54:21 -0700

    Codes up through Episode 20 of Game Programming.

--- src/main/java/com/artbeatte/rain/graphics/SpriteSheet.java
+++ src/main/java/com/artbeatte/rain/graphics/SpriteSheet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class SpriteSheet {
 
     private String path;
-    private final int SIZE;
+    public final int SIZE;
     public int[] pixels;
 
     public SpriteSheet(String path, int size) {
