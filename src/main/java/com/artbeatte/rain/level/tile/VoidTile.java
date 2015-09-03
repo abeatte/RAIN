808c6f26d7d01c7bb98a5bc54842ddc2302a1205
Art Beatte <art.beatte@docusign.com>
2015-09-03 10:56:06 -0700

    Codes up through Episode 34 of Game Programming.

--- src/main/java/com/artbeatte/rain/level/tile/VoidTile.java
+++ src/main/java/com/artbeatte/rain/level/tile/VoidTile.java
@@ -1,8 +1,19 @@
 package com.artbeatte.rain.level.tile;
 
+import com.artbeatte.rain.graphics.Screen;
+import com.artbeatte.rain.graphics.Sprite;
+
 /**
  * @author art.beatte
  * @version 9/3/15
  */
-public class VoidTile {
+public class VoidTile extends Tile {
+
+    public VoidTile(Sprite sprite) {
+        super(sprite);
+    }
+
+    public void render(int x, int y, Screen screen) {
+        screen.renderTile(x, y, this);
+    }
 }
