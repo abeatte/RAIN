d40f54fffc06734d62dbe8b210df3a0d44759813
Art Beatte <art.beatte@docusign.com>
2015-09-03 15:43:01 -0700

    Codes up through Episode 39 of Game Programming.

---
+++ src/main/java/com/artbeatte/rain/entity/Entity.java
@@ -0,0 +1,33 @@
+package com.artbeatte.rain.entity;
+
+import com.artbeatte.rain.graphics.Screen;
+import com.artbeatte.rain.level.Level;
+
+import java.util.Random;
+
+/**
+ * @author art.beatte
+ * @version 9/3/15
+ */
+public abstract class Entity {
+
+    public int x, y;
+    private boolean removed = false;
+    protected Level level;
+    protected final Random random = new Random();
+
+    public void update() {
+    }
+
+    public void render(Screen screen) {
+    }
+
+    public void remove() {
+        // Remove from level
+        removed = true;
+    }
+
+    public boolean isRemoved() {
+        return removed;
+    }
+}
