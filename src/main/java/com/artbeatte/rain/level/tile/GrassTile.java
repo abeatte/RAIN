b7082e2cd8149954a9dc84cfe0f3e9273fee6c5b
Art Beatte <art.beatte@docusign.com>
2015-09-03 09:54:17 -0700

    Codes up through Episode 27 of Game Programming.

---
+++ src/main/java/com/artbeatte/rain/level/tile/GrassTile.java
@@ -0,0 +1,19 @@
+package com.artbeatte.rain.level.tile;
+
+import com.artbeatte.rain.graphics.Screen;
+import com.artbeatte.rain.graphics.Sprite;
+
+/**
+ * @author art.beatte
+ * @version 9/3/15
+ */
+public class GrassTile extends Tile {
+
+    public GrassTile(Sprite sprite) {
+        super(sprite);
+    }
+
+    public void render(int x, int y, Screen screen) {
+        // Do render stuff here!
+    }
+}
--- src/main/java/com/artbeatte/rain/level/tile/Tile.java
+++ src/main/java/com/artbeatte/rain/level/tile/Tile.java
@@ -12,6 +12,8 @@ public class Tile {
     public int x, y;
     public Sprite sprite;
 
+    public static Tile grass = new GrassTile(Sprite.grass);
+
     public Tile(Sprite sprite) {
         this.sprite = sprite;
     }
