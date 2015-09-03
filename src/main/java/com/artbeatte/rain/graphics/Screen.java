2b828dd7558e9d58d73a454b7aba3b982dc21c76
Art Beatte <art.beatte@docusign.com>
2015-09-03 09:17:52 -0700

    Codes up through Episode 22 of Game Programming.

--- src/main/java/com/artbeatte/rain/graphics/Screen.java
+++ src/main/java/com/artbeatte/rain/graphics/Screen.java
@@ -42,7 +42,7 @@ public class Screen {
                 int xx = x + xOffset;
 //                if (xx >= width || xx < 0) break;
                 int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
-                pixels[x + y * width] = tiles[tileIndex];
+                pixels[x + y * width] = Sprite.grass.pixels[(x&15) + (y&15) * Sprite.grass.SIZE];
             }
         }
     }
