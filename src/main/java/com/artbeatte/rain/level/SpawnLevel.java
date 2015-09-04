7389b406a0116f778743396c2236ed067d35cfa7
Art Beatte <art.beatte@docusign.com>
2015-09-03 21:02:30 -0700

    Codes up through Episode 56 of Game Programming.

--- src/main/java/com/artbeatte/rain/level/SpawnLevel.java
+++ src/main/java/com/artbeatte/rain/level/SpawnLevel.java
@@ -1,7 +1,5 @@
 package com.artbeatte.rain.level;
 
-import com.artbeatte.rain.level.tile.Tile;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -12,8 +10,6 @@ import java.io.IOException;
  */
 public class SpawnLevel extends Level {
 
-    private int[] levelPixels;
-
     public SpawnLevel(String path) {
         super(path);
     }
@@ -23,23 +19,16 @@ public class SpawnLevel extends Level {
             BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
             int w = image. getWidth();
             int h = image.getHeight();
-            tiles = new Tile[w * h];
-            levelPixels = new int[w * h];
-            image.getRGB(0, 0, w, h, levelPixels, 0, w);
+            tiles = new int[w * h];
+            image.getRGB(0, 0, w, h, tiles, 0, w);
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Exception! Could not load lavel file!");
         }
     }
 
-    // Grass  = 0xFF00FF00
-    // Flower = 0xFFFFFF00
-    // Rock   = 0xFF7F7F00
+
     protected void generateLevel() {
-        for (int i = 0; i < levelPixels.length; i++) {
-            if (levelPixels[i] == 0xff00ff00) tiles[i] = Tile.grass;
-            if (levelPixels[i] == 0xffffff00) tiles[i] = Tile.flower;
-            if (levelPixels[i] == 0xff7f7f00) tiles[i] = Tile.rock;
-        }
+
     }
 }
