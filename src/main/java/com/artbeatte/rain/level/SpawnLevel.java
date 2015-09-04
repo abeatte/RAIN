93457c31cd26762fe4d639287b8faf85d5f8ece9
Art Beatte <art.beatte@docusign.com>
2015-09-03 20:30:13 -0700

    Codes up through Episode 53 of Game Programming.

--- src/main/java/com/artbeatte/rain/level/SpawnLevel.java
+++ src/main/java/com/artbeatte/rain/level/SpawnLevel.java
@@ -24,6 +24,7 @@ public class SpawnLevel extends Level {
             BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
             int w = image. getWidth();
             int h = image.getHeight();
+            tiles = new Tile[w * h];
             image.getRGB(0, 0, w, h, levelPixels, 0, w);
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,7 +32,14 @@ public class SpawnLevel extends Level {
         }
     }
 
+    // Grass = 0xFF00
+    // Flower = 0xFFF00
+    // Rock = 0x7F7F00
     protected void generateLevel() {
-        
+        for (int i = 0; i < levelPixels.length; i++) {
+            if (levelPixels[i] == 0xff00) tiles[i] = Tile.grass;
+            if (levelPixels[i] == 0xffff00) tiles[i] = Tile.flower;
+            if (levelPixels[i] == 0x7f7f00) tiles[i] = Tile.rock;
+        }
     }
 }
