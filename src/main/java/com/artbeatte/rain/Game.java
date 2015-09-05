68e6977d9608ae4f8d98fb4de155f65ee8bbfc5c
Art Beatte <art.beatte@docusign.com>
2015-09-04 20:38:29 -0700

    Codes up through Episode 60 of Game Programming.

--- src/main/java/com/artbeatte/rain/Game.java
+++ src/main/java/com/artbeatte/rain/Game.java
@@ -43,7 +43,7 @@ public class Game extends Canvas implements Runnable {
         screen = new Screen(width, height);
         frame = new JFrame();
         key = new Keyboard();
-        level = new SpawnLevel("/textures/levels/level.png");
+        level = new SpawnLevel("/levels/level.png");
         player = new Player(250, 100, key);
 
         addKeyListener(key);
